7993a21f32ec36723c925d9432ad74425b3a2ee0
Danesed <danese.d@live.it>
2021-09-28 17:43:38 +0200

    aggiunto: rinominare file output

--- PyhonDVD.py
+++ PyhonDVD.py
@@ -1,10 +1,9 @@
 f_original = input ("Quale file? trascinalo :")
 f_original = f_original.replace("'","").strip() #rimuove apici dovuti al trascinamento del percorso del 'file//esempio//'
 with open(f_original,encoding='utf-8',errors='replace') as original:
-#with open('1.txt',encoding='utf-8',errors='replace') as original:
     num = input ("Quanti cm ? (es 4mm =0.4) :")
     print(num)
-    with open("out.txt", "w") as f:
+    with open(f_original[:-4]+"_out.txt", "w") as f: #crea un nuovo file col nome fileoriginale_old, andando ad escludere la porzione ".txt" del file
         for line in original:
             if '; layer' not in line: #scrive linee senza ; layer
                 f.write(line);
@@ -16,7 +15,7 @@ with open(f_original,encoding='utf-8',errors='replace') as original:
                         Z_axis = float(res.split()[3]) #estrae da res solo il numero e lo casta in float
                         f.write(line.replace(str(Z_axis),str(Z_axis + float(num)))) #rimpiazza il numero con il numero modificato con la costante
                         #print(line)
-                        #print(res)
+                        print("Layer: ",res," --> ", Z_axis + float(num))
                         #print(layer_num)
                     elif (layer_num % 2) != 0: #se il numero è dispari lo stampa e basta
                         f.write(line)
